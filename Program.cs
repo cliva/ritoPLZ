3e6dd60a3180b8ca76e3227d37b8c36900323b85
JackBruzan <jackbruzan@gmail.com>
2019-04-04 19:23:59 -0500

    Added basic game downloading

--- Program.cs
+++ Program.cs
@@ -9,32 +9,7 @@ namespace riot
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            MLBApi mlbapi = new MLBApi();
-            //var divisionsJSON = mlbapi.DownloadDivisions();
-            //Console.WriteLine(divisionsJSON);
-            Task t = new Task(DownloadDivisions2);
-            t.Start();
-            
-            
-            Schedule schedule = new Schedule();
-            
-            Console.ReadLine();
-        }
-
-        static async void DownloadDivisions2() {
-            string page = "http://statsapi.mlb.com/api/v1/schedule/?sportId=1";
-            using (HttpClient client = new HttpClient()){
-                using (HttpResponseMessage response = await client.GetAsync(page)){
-                    using (HttpContent content = response.Content){
-                        string result = await content.ReadAsStringAsync();
-                        if (result != null && result.Length >= 50)
-                            using(StreamWriter sw = new StreamWriter("/users/jackbruzan/desktop/baseball/schedule.json")){
-                                sw.WriteLine(result);
-                            }
-                    }
-                }
-            }
+            Game game = new Game(2936546794);
         }
     }
 }
